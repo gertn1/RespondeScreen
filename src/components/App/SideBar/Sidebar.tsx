f8673dfd4fb9e25e92ec3262e31d1493223e6784
Germano Ribeiro <germanno.verde@gmail.com>
2024-06-05 12:13:53 -0300

    k

--- src/components/App/SideBar/Sidebar.tsx
+++ src/components/App/SideBar/Sidebar.tsx
@@ -1,138 +1,30 @@
 
-
-// // import React from 'react';
-// // import { NavLink } from 'react-router-dom';
-// // import styled from 'styled-components';
-// // import { menuItems } from './Menusidebar/MenuItensSidebar'; 
-
-
-// // // Definição de styled-components para o container do Sidebar
-// // const SidebarContainer = styled.div<{ isOpen: boolean }>`
-// //   width: ${(props) => (props.isOpen ? '250px' : '60px')};  // A largura é dinâmica com base na prop isOpen
-// //   height: 100vh;
-// //   transition: width 0.3s;
-// //   overflow-x: hidden;
-// //   position: fixed;
-// //   top: 0;
-// //   left: 0;
-// //   background-color: #282c34;
-// //   color: white;
-// // `;
-
-// // const SidebarMenuItems = styled.div`
-// //   display: flex;
-// //   flex-direction: column;
-// // `;
-
-// // const SidebarItem = styled(NavLink)`
-// //   display: flex;
-// //   align-items: center;
-// //   padding: 10px 20px;
-// //   text-decoration: none;
-// //   color: white;
-
-// //   &:hover {
-// //     background-color: #555;
-// //   }
-// // `;
-
-
-
-
-// // export const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> = ({ isOpen }) => {
-// //   return (
-// //     <SidebarContainer isOpen={isOpen}>
-// //       {menuItems.map(({ label, url }) => (
-// //         <SidebarMenuItems key={label}>
-// //           <SidebarItem to={url}>
-// //             {isOpen && <span>{label}</span>} 
-// //           </SidebarItem>
-// //         </SidebarMenuItems>
-// //       ))}
-// //     </SidebarContainer>
-// //   );
-// // };
-
-
-// import React from 'react';
-// import { NavLink } from 'react-router-dom';
-// import styled from 'styled-components';
-// import { menuItems } from './Menusidebar/MenuItensSidebar';
-// import { IonIcon } from '@ionic/react';  // Certifique-se de que está importando o componente correto para os ícones
-
-// // Definição de styled-components para o container do Sidebar
-// const SidebarContainer = styled.div<{ isOpen: boolean }>`
-//   width: ${(props) => (props.isOpen ? '250px' : '60px')};
-//   height: 100vh;
-//   transition: width 0.3s;
-//   overflow-x: hidden;
-//   position: fixed;
-//   top: 0;
-//   left: 0;
-//   background-color: #282c34;
-//   color: white;
-// `;
-
-// const SidebarMenuItems = styled.div`
-//   display: flex;
-//   flex-direction: column;
-// `;
-
-// const SidebarItem = styled(NavLink)`
-//   display: flex;
-//   align-items: center;
-//   padding: 10px 20px;
-//   text-decoration: none;
-//   color: white;
-
-//   &:hover {
-//     background-color: #555;
-//   }
-
-//   svg {
-//     margin-right: ${props => props.isOpen ? '10px' : '0'};
-//     transition: margin-right 0.3s;
-//   }
-// `;
-
-// export const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> = ({ isOpen }) => {
-//   return (
-//     <SidebarContainer isOpen={isOpen}>
-//       {menuItems.map(({ label, url, icon }) => (
-//         <SidebarMenuItems key={label}>
-//           <SidebarItem to={url}>
-//             <IonIcon icon={icon} />
-//             {isOpen && <span>{label}</span>}
-//           </SidebarItem>
-//         </SidebarMenuItems>
-//       ))}
-//     </SidebarContainer>
-//   );
-// };
-
-
 import React from 'react';
 import styled from 'styled-components';
 import { menuItems } from './Menusidebar/MenuItensSidebar';
 import { IonIcon } from '@ionic/react';
-import SidebarItem from './SidebarItem/SidebarItem';  // Certifique-se de que o caminho está correto
+import SidebarItem from './SidebarItem/SidebarItem';  
 
-// Definição de styled-components para o container do Sidebar
+
 const SidebarContainer = styled.div<{ isOpen: boolean }>`
   width: ${(props) => (props.isOpen ? '250px' : '60px')};
   height: 100vh;
   transition: width 0.3s;
   overflow-x: hidden;
   position: fixed;
-  top: 0;
+  top: 0px;
   left: 0;
   background-color: #282c34;
   color: white;
+  padding-top: 50px
+  
+
 `;
 
 const SidebarMenuItems = styled.div`
   display: flex;
   flex-direction: column;
+  font-size: 1.3rem;
 `;
 
 export const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> = ({ isOpen }) => {
@@ -142,7 +34,7 @@ export const Sidebar: React.FC<{ isOpen: boolean; toggleSidebar: () => void }> =
         <SidebarMenuItems key={label}>
           <SidebarItem to={url} isOpen={isOpen}>
             <IonIcon icon={icon} />
-            {isOpen && <span>{label}</span>}
+            {isOpen && <span style={{ marginLeft: '10px' }}>{label} </span>}
           </SidebarItem>
         </SidebarMenuItems>
       ))}
