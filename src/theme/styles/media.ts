d9714603d8380af44c4c6a4460275db3708ed0b7
Germano Ribeiro <germanno.verde@gmail.com>
2024-06-10 11:25:35 -0300

    d

--- src/theme/styles/media.ts
+++ src/theme/styles/media.ts
@@ -9,16 +9,3 @@ const sizes = {
     phone: `(max-width: ${sizes.phone}px)`,
   };
   
-
-  // const sizes = {
-  //   desktop: 1024,
-  //   tablet: 750,
-  //   phone: 576,
-  // };
-  
-  // export const media = {
-  //   desktop: `(min-width: ${sizes.desktop}px)`,
-  //   tablet: `(max-width: ${sizes.tablet}px)`,
-  //   phone: `(max-width: ${sizes.phone}px)`,
-  // };
-  
